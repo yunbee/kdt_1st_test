77a6fd454f03b35e7ccf8f486bb202a5590f7282
yunbee <106522938+yunbee@users.noreply.github.com>
2022-07-19 20:07:48 +0900

    Delete ex_1.js

--- schedule/ex_1.js
+++
@@ -1,9 +0,0 @@
-const dateEl = document.getElementById("date");
-const contentEl = document.getElementById("content");
-const buttonEl = document.querySelector("table");
-const btn = document.querySelector("button");
-
-
-buttonEl.addEventListener("click", function (e) {
-    dateEl.value = e.target.innerText;
-})
